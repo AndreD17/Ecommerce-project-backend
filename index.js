5209d780f0f4f82ed4b940b63da01b61cc14ceb7
Andred17 <Damilaresammy1996@gmail.com>
2025-11-03 16:16:12 +0100

    Index.js file updated CORS

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://your-frontend-domain.onrender.com",
-    "https://your-admin-domain.onrender.com"
+    "https://ecommerce-project-admin-uuwx.onrender.com/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
