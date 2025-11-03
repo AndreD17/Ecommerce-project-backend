93cf33acefefa8dd1ef3acc473f7e83e1d193852
Andred17 <Damilaresammy1996@gmail.com>
2025-11-03 15:42:18 +0100

    Index.js file updated

--- index.js
+++ index.js
@@ -15,16 +15,11 @@ import cartRoutes from "./routes/cartRoutes.js";
 const app = express();
 const PORT = process.env.PORT;
 
-// __dirname & __filename for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Middleware
 app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://ecommerce-project-yofl.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
@@ -37,12 +32,6 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static images
 app.use("/images", express.static(path.join(__dirname, "upload/images")));
 
-// Serve frontend build
-app.use("/", express.static(path.join(__dirname, "../frontend/build")));
-
-
-// Serve admin build
-app.use("/admin", express.static(path.join(__dirname, "../admin/build")));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -53,20 +42,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-
-
-// SPA fallback for frontend (any route that is not /api or /admin)
-app.get(/^\/(?!api|admin).*$/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
-
-// SPA fallback for admin: any route starting with /admin but not /api
-app.get(/^\/admin(\/.*)?$/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../admin/build/index.html'));
-});
-
-
-
 
 // Test route
 app.get("/", (req, res) => res.send("Ecommerce API is running 🚀"));
